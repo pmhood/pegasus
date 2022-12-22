e7405da15ca5dba45a862163304466616970f498
Peter Hood <pmhood@gmail.com>
2022-12-22 16:36:40 -0500

    Added month, removed toast-ui

---
+++ src/client/components/Month.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import moment, { type Moment } from 'moment';
+import { ref } from 'vue';
+
+const startWeek = moment().startOf('month').week();
+const endWeek = moment().endOf('month').startOf('day').week();
+const numWeeks = endWeek - startWeek + 1;
+
+const calendarDates = [] as Moment[];
+for (let i = 0; i < numWeeks * 7; i++) {
+  calendarDates.push(moment().startOf('month').startOf('week').add(i, 'd'));
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-end">
+    <div class="max-w-sm w-full">
+      <div class="md:p-8 p-5 dark:bg-gray-800 bg-white rounded-t">
+        <div class="flex items-center justify-center">
+          <span
+            tabindex="0"
+            class="focus:outline-none text-2xl font-bold dark:text-gray-100 text-gray-800"
+            >{{ moment().format('MMMM') }}</span
+          >
+        </div>
+        <div class="flex items-center justify-between pt-12 overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr>
+                <th v-for="idx in 7">
+                  <div class="w-full flex justify-center">
+                    <p
+                      class="text-base font-medium text-center text-gray-800 dark:text-gray-100"
+                    >
+                      {{
+                        moment()
+                          .day(idx - 1)
+                          .format('dd')
+                      }}
+                    </p>
+                  </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="weekNum in numWeeks">
+                <td
+                  v-for="(item, index) in calendarDates.slice(
+                    (weekNum - 1) * 7,
+                    (weekNum - 1) * 7 + 7
+                  )"
+                >
+                  <div
+                    class="px-2 py-2 cursor-pointer flex w-full justify-center"
+                  >
+                    <p
+                      class="text-base text-gray-500 dark:text-gray-100 font-medium"
+                      :class="{
+                        nonCurrentMonth: item.month() !== moment().month(),
+                        today: moment(item).isSame(moment(), 'd')
+                      }"
+                    >
+                      {{ item.format('D') }}
+                    </p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nonCurrentMonth {
+  @apply text-slate-200;
+}
+.today {
+  @apply focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-700 focus:bg-indigo-500 hover:bg-indigo-500 text-base w-8 h-8 flex items-center justify-center font-medium text-white bg-indigo-700 rounded-full;
+}
+</style>
--- src/client/components/widgets/calendar/CalendarWidget.vue
+++ src/client/components/widgets/calendar/CalendarWidget.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import router from '@/router';
 import { onMounted, ref } from 'vue';
+import Month from '../../Month.vue';
 
 // export interface PhotoOfTheDayData {
 //   imageUrl: string;
@@ -14,7 +15,10 @@ function go() {
 }
 </script>
 <template>
-  <div class="bg-red-200 cursor-pointer" @click="go()">
+  <div class="bg-white cursor-pointer grid grid-cols-2" @click="go()">
     <h1>Calendar View</h1>
+    <div class="">
+      <Month class=""></Month>
+    </div>
   </div>
 </template>
