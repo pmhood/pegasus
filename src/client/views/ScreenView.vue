492622495a8a11159b909b51c5dfed893a872423
Peter Hood <pmhood@gmail.com>
2023-01-05 21:43:09 -0500

    Added screen refresh

--- src/client/views/ScreenView.vue
+++ src/client/views/ScreenView.vue
@@ -2,20 +2,34 @@
 import axios from 'axios';
 import { componentClassFromString } from '@/services/component-map';
 import type { ScreenResponseData } from 'src/common/dto/screen-response-data';
+import { ref } from 'vue';
 
-const response = await axios.get('/api/screens/home');
-const screenResponse = response.data as ScreenResponseData;
+let screenResponse = ref<ScreenResponseData>();
+
+await refreshScreen();
+
+async function refreshScreen() {
+  const response = await axios.get('/api/screens/home');
+  screenResponse.value = response.data as ScreenResponseData;
+
+  if (screenResponse.value.refreshInterval) {
+    setTimeout(() => {
+      refreshScreen();
+    }, screenResponse.value.refreshInterval);
+  }
+}
 </script>
 
 <template>
-  <div class="bg-red-500">
+  <div class="">
     <component
+      v-if="screenResponse"
       v-bind:is="componentClassFromString(screenResponse.layout)"
       v-bind="screenResponse"
       class="h-full"
     ></component>
     <div class="absolute top-0 right-0 text-white text-xs">
-      v{{ screenResponse.version }}
+      v{{ screenResponse?.version }}
     </div>
   </div>
 </template>
